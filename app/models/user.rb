ae87f3ef3e25046509e38a7ab2cf380fbc1375d7
Julián <judramirezva@gmail.com>
2019-08-01 19:22:01 -0500

    Ticket #13 - Add users & roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :city
+
+  after_create :set_role
   after_create :send_welcome_email
+  
+  enum role: [ :user, :student, :teacher, :admin ]
+
+  def set_role
+  	self.user!
+	end
 
   def send_welcome_email
   	UserNotifierMailer.welcome_user(self).deliver_now
