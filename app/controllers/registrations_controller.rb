14b494657def4a6bded85f161ab9b56f2cfc2d8f
Julián <judramirezva@gmail.com>
2019-08-02 19:34:50 -0500

    Ticket #20 - Route change

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 	protected
 	  def after_sign_up_path_for(resource)
 	  	if params[:role] == "student"
+	  		####
 	  		new_student_path(role: params[:role])
 	  	elsif params[:role] == "teacher"
 	  		root_path(role: params[:role])
